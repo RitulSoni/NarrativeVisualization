ca86f87101f447b5b1ae5a343bae671c2231bc7a
Ritul Soni <rsoni27@illinois.edu>
2023-07-30 16:09:00 -0500

    made bubble chart responsive

--- src/BubbleChart.js
+++ src/BubbleChart.js
@@ -26,8 +26,11 @@ async function processData() {
     });
 }
 
+
 // Drawing chart
 async function drawBubbleChart() {
+  d3.select("#bubbleChart").selectAll("*").remove(); // Clear previous chart
+
   const data = await processData();
   
   const groupedData = d3.group(data, d => d.OCC_TITLE);
@@ -48,10 +51,16 @@ async function drawBubbleChart() {
 
   jobLossData = jobLossData.sort((a,b) => d3.ascending(a.percentageChange, b.percentageChange)).slice(0, 10);
 
-  const svg = d3.select("#bubbleChart").attr("viewBox", `0 0 960 500`),
-      margin = {top: 60, right: 80, bottom: 60, left: 400},
-      width = 960 - margin.left - margin.right,
-      height = 500 - margin.top - margin.bottom;
+  const svgWidth = window.innerWidth * 0.8, // Adjust as needed
+        svgHeight = window.innerHeight * 0.5, // Adjust as needed
+        margin = {top: 60, right: 40, bottom: 60, left: 650},
+        width = svgWidth - margin.left - margin.right,
+        height = svgHeight - margin.top - margin.bottom;
+
+  const svg = d3.select("#bubbleChart")
+                .attr("width", svgWidth)
+                .attr("height", svgHeight)
+                .attr("viewBox", `0 0 ${svgWidth} ${svgHeight}`);
 
   const g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
@@ -64,26 +73,53 @@ async function drawBubbleChart() {
   yScale.domain(jobLossData.map(d => d.job));
   rScale.domain([0, Math.max(Math.abs(d3.min(jobLossData, d => d.percentageChange)), d3.max(jobLossData, d => d.percentageChange))]);
 
+
+  // Adjust x axis
   g.append("g")
       .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(xScale).tickFormat(d3.format(".2s")))
-      .append("text") // More descriptive label
+      .call(d3.axisBottom(xScale).tickFormat(d3.format(".2s")).tickSize(10)) // Adjust tick size
+      .selectAll("text")  
+      .style("text-anchor", "end")
+      .attr("dx", "-.8em")
+      .attr("dy", ".15em")
+      .attr("transform", "rotate(-65)")  
+      .style("font-size", "14px"); // Adjust label size
+  g.select(".domain")
+      .style("stroke-width", "2"); // Adjust axis line thickness
+      
+  // If you also want to adjust the font size of the axis labels, you can do so as follows:
+  g.append("text") // More descriptive label
       .attr("x", width / 2)
-      .attr("y", margin.bottom / 2)
+      .attr("y", 450)
       .attr("fill", "black")
       .style("text-anchor", "middle")
+      .style("font-size", "18px") // Adjust this value
       .text("Percentage Change in Employment");
 
-  g.append("g")
-      .call(d3.axisLeft(yScale))
-      .append("text") // More descriptive label
+  // Adjust y axis
+  var yAxis = g.append("g")
+      .call(d3.axisLeft(yScale)); // Generate y-axis
+
+  yAxis.selectAll("text")  
+      .style("font-size", "14px"); // Adjust label size
+
+  yAxis.select(".domain")
+      .style("stroke-width", "2"); // Adjust axis line thickness
+
+  g.append("text") // More descriptive label
       .attr("x", -height / 2)
-      .attr("y", -385)
+      .attr("y", -550)
       .attr("transform", "rotate(-90)")
       .attr("fill", "black")
       .style("text-anchor", "middle")
+      .style("font-size", "18px") // Adjust this value
       .text("Job Titles");
 
+      
+
+
+
+      //This is the top title 
   g.append("text")
       .attr("x", width / 2)
       .attr("y", -margin.top / 2)
@@ -91,6 +127,9 @@ async function drawBubbleChart() {
       .style("font-size", "24px")
       .style("font-weight", "bold")
       .text("Top 10 Job Losses");
+
+
+
 
   const tooltip = d3.select("body").append("div")
       .attr("class", "tooltip")
@@ -162,3 +201,5 @@ async function drawBubbleChart() {
 }
 
 drawBubbleChart();
+
+window.addEventListener("resize", drawBubbleChart);
